a5f4cb536963bb1555ee27f59acf983571cabc70
zm412 <zm412412@gmail.com>
2021-03-09 15:48:27 -0500

    added new func for checking case of words. Still working on it

--- components/translator.js
+++ components/translator.js
@@ -22,28 +22,61 @@ class Translator {
       answer.translation = this.americanToBritish(objBody.text)
     }
     console.log(answer)
+    //console.log(this.caseChecking('Mr.', this.dictTitle), 'caseChecking')
     return answer;
   }
+
 
   americanToBritish(str){
     let sentenceArr = str.split(' ')  ;
     let newSentenceArr = [];
 
     for(let i = 0; i < sentenceArr.length; i++){
-      if(this.dictSpell.hasOwnProperty(sentenceArr[i])){
-        newSentenceArr.push(this.dictSpell[sentenceArr[i]])
-      }else if(this.dictTitle.hasOwnProperty(sentenceArr[i])){
-        newSentenceArr.push(this.dictTitle[sentenceArr[i]])
-      }else if(this.americanOnly.hasOwnProperty(sentenceArr[i])){
-        newSentenceArr.push(this.americanOnly[sentenceArr[i]])
+      
+      if(sentenceArr.length == 1){
+        let tempArr = sentenceArr[0].split('')  ;
+        tempArr[0] = tempArr[0].toUpperCase();
+        return tempArr.join('');
+
+      }else if(this.checkingCase(sentenceArr[i], this.dictSpell)){
+        newSentenceArr.push(this.checkingCase(sentenceArr[i], this.dictSpell));
+      }else if(this.checkingCase(sentenceArr[i], this.dictTitle)){
+        newSentenceArr.push(this.checkingCase(sentenceArr[i], this.dictTitle))
+      }else if(this.checkingCase(sentenceArr[i], this.americanOnly)){
+        newSentenceArr.push(this.checkingCase(sentenceArr[i], this.americanOnly))
       }else{
-        newSentenceArr.push(sentenceArr[i])
+        newSentenceArr.push(this.checkingCase(sentenceArr[i]))
       }
 
     }
     console.log(newSentenceArr, 'sentence')
     return newSentenceArr.join(' ');
   }
+
+
+  checkingCase(str, obj){
+    //let arrSting = str.split('');
+    //let isCaseLow = arrString[0] == arrString[0].toLowerCase();
+    let isCaseLow = str[0] == str[0].toLowerCase();
+    let lowCaseStr = str.toLowerCase();
+    let changedStr, arrStr;
+    
+
+    if(!obj){
+      //arrStr = str.split('');
+      //arrStr[0] = arrStr[0].toUpperCase();
+//      changedStr = arrStr.join('');
+      changedStr = str;
+    }else if(obj.hasOwnProperty(lowCaseStr) && isCaseLow){
+      changedStr = obj[str];
+    }else if(obj.hasOwnProperty(lowCaseStr) && !isCaseLow){
+      arrStr = obj[lowCaseStr].split('');
+      arrStr[0] = arrStr[0].toUpperCase();
+      changedStr = arrStr.join('')
+    }
+
+    return changedStr;
+    }
 
 }
 
